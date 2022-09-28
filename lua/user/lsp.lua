ed9f827e581bc2aff3f09ebf69dbff3fa0f16abd
pellepedro <Per.Pettersson@gmail.com>
2022-09-28 13:54:19 -0400

    added codespell linter + null-ls

--- lua/user/lsp.lua
+++ lua/user/lsp.lua
@@ -26,8 +26,24 @@ lvim.builtin.treesitter.ensure_installed = {
 -- vim.list_extend(lvim.lsp.automatic_configuration.skipped_servers, { "jdtls", "gopls" })
 
 -- set a formatter, this will override the language server formatting capabilities (if it exists)
--- local formatters = require "lvim.lsp.null-ls.formatters"
--- formatters.setup {
---   { command = "google_java_format", filetypes = { "java" } },
---   { command = "stylua", filetypes = { "lua" } },
--- }
+local formatters = require "lvim.lsp.null-ls.formatters"
+formatters.setup {
+  {
+    command = "codespell",
+    filetypes = { "go", "javascript", "python" },
+  },
+  { command = "stylua", filetypes = { "lua" } },
+}
+
+local linters = require "lvim.lsp.null-ls.linters"
+linters.setup {
+  {
+    command = "codespell",
+    filetypes = { "go", "javascript", "python" },
+  },
+  { command = "flake8", filetypes = { "python" } },
+  {
+    command = "shellcheck",
+    extra_args = { "--severity", "warning" },
+  },
+}
